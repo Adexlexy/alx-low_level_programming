59bd57dc4ed7f14ab652a5059085c15b8b876441
Adexlexy <oshewolo_martins@yahoo.com>
2022-10-13 14:22:59 -0700

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	printf("Size of a char: %Id byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %Id byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %Id byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %Id byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %Id byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %Id byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
