35af1161f05fe75f20fe3fcf36d6fe4f37323346
Adexlexy <oshewolo_martins@yahoo.com>
2022-10-14 12:12:47 -0700

    C2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,6 @@ int main(void)
 
 	x = n % 10;
 	printf("Last digit of %d is %d ", n, x);
-	
 	if (x == 0)
 	{
 		printf("and is 0");
@@ -29,10 +28,10 @@ int main(void)
 	}
 	if (x < 6 && x != 0)
 	{
-		printf("and is less than 6 but not 0");
+		printf("and is less than 6 and not 0");
 	}
-	
 	printf("\n");
+
 
 	return (0);
 }
