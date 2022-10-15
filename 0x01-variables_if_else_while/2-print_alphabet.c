0991e8b5b96edb316b844219d32f77a56b1b54ee
Adexlexy <oshewolo_martins@yahoo.com>
2022-10-15 09:48:44 -0700

    C2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 /**
  * main -Entry point
  * Return: Always 0
@@ -9,6 +9,6 @@ int main(void)
 
 	for (low = 'a'; low <= 'z'; low++)
 		putchar (low);
-	putchar ('\n')
-	return (0)
+	putchar ('\n');
+	return (0);
 }
