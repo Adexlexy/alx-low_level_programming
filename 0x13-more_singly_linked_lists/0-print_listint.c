3d2b831f0185775ca67f2499466bb27736b49a06
Adexlexy <oshewolo_martins@yahoo.com>
2022-11-29 06:55:49 -0800

    More_singly

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,6 +5,7 @@
  * print_listint - prints all the elements of a listint_t list.
  *
  * @h: head of linklist node
+ *
  * Return: the number of nodes
  *
  */
@@ -12,12 +13,7 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;	
 	
-	if (h == NULL)
-	{
-		return (-1);
-	}
-	else
-		(h != NULL);
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
